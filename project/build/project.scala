b1ef4eb954401398e010daf66624692901343694
Philip Cali <philip.cali@gmail.com>
2011-01-03 08:22:15 -0600

    Added an executable script for linux machines.

--- project/build/project.scala
+++ project/build/project.scala
@@ -23,4 +23,29 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val uff = "net.databinder" %% "unfiltered-filter" % "0.2.0"
   val ufs = "net.databinder" %% "unfiltered-scalate" % "0.2.0"
   val ufu = "net.databinder" %% "unfiltered-uploads" % "0.2.0"
+
+  override def mainClass = Some("com.philipcali.cct.Converter")
+
+  override def packageAction = task { 
+    packageTask(packagePaths, jarPath, packageOptions)
+    val command = "zip -r %s/program.zip %s %s" format (outputPath, jarPath, managedDependencyPath)
+    Runtime.getRuntime().exec(command)
+    createScript()
+    Runtime.getRuntime().exec("chmod +x %s" format(outputPath / "cct"))
+    None
+  } dependsOn(compile) describedAs("Packages jar, plus creates a cct script for server machines")
+
+  def createScript() {
+    val jarSplit = jarPath.toString.split("/")
+    val manSplit = managedDependencyPath.toString.split("/")
+
+    def prepender(xs: Array[String]) = "cct_program/" + xs.drop(1).mkString("/")
+
+    val writer = new java.io.FileWriter((outputPath / "cct").toString)
+    writer.write("#! /bin/sh\n")
+    writer.write("unzip -qq program.zip -d cct_program\n")
+    writer.write("scala -classpath %s:%s/compile/* %s \"$@\"\n" format(prepender(jarSplit), prepender(manSplit), mainClass.get))
+    writer.write("rm -rf cct_program") 
+    writer.close()
+  }
 }
